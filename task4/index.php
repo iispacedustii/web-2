d7c56ba4ca1d33e7918b1ff4e3a25092cc9321c1
Ivan Vlasenko <144020773+iispacedustii@users.noreply.github.com>
2024-05-14 13:19:04 +0300

    Update index.php

--- task4/index.php
+++ task4/index.php
@@ -11,74 +11,73 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   include('form.html');
   exit();
 }
-
-// Проверяем ошибки.
-$errors = FALSE;
-
-
-if ( empty($_POST['name']) || !preg_match('/([A-Za-zА-Яа-я-]+\s[A-Za-zА-Яа-я-]+\s[A-Za-zА-Яа-я-]+)/', $_POST['name'])) {
-  print('ФИО не указаны!<br/>');
-  $errors = TRUE;
-}
-
-if ( empty($_POST['phone']) || !is_numeric($_POST['phone'])) {
-  print('Номер телефона не указан!<br/>');
-  $errors = TRUE;
-}
-
-if ( empty($_POST['email']) ) {
-  print('Почта не указана!<br/>');
-  $errors = TRUE;
-}
-
-if ( empty($_POST['date']) ) {
-  print('Дата рождения не указана!<br/>');
-  $errors = TRUE;
-}
-if ( !isset($_POST['sex']) || !in_array($_POST['sex'], array('male', 'female')) ) {
-  print('Пол не указан!<br/>');
-  $errors = TRUE;
-}
-
-if ( empty($_POST['langs']) ) {
-  print('Языки программирования не выбраны!<br/>');
-  $errors = TRUE;
-}
-
-if (empty($_POST['bio']) ) {
-  print('Заполните биографию.<br/>');
-  $errors = TRUE;
-}
-
-if(!isset($_POST['checkmark']) || $_POST['checkmark'] != 'on') {
-  print('Отметьте чекбокс.<br/>');
-  $errors = TRUE;
-}
-
-if ($errors) {
-  exit();
-}
-
-// Сохранение в базу данных.
-
-$user = 'u67354';
-$pass = '3075308';
-$db = new PDO('mysql:host=localhost;dbname=u67354', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
-// Подготовленный запрос. Не именованные метки.
-try {
-    $stmt = $db->prepare('INSERT INTO form SET name = ?, phone = ?, email = ?, date = ?, sex = ?, bio = ?');
-    $stmt -> execute(array( $_POST['name'], $_POST['phone'], $_POST['email'], $_POST['date'], $_POST['sex'], $_POST['bio'] ));
+else {
+  $errors = FALSE;
   
-    $id = $db->lastInsertId();
-    
-    foreach ($_POST['langs'] as $lang) {
-      $stmt = $db->prepare('INSERT INTO lang_table (id, lang) VALUES (?, ?)');
-      $stmt->execute([$id, $lang]);
-    }
+  if ( empty($_POST['name']) || !preg_match('/([A-Za-zА-Яа-я-]+\s[A-Za-zА-Яа-я-]+\s[A-Za-zА-Яа-я-]+)/', $_POST['name'])) {
+    print('ФИО не указаны!<br/>');
+    $errors = TRUE;
   }
-  catch(PDOException $e){
-    print('Ошибка: ' . $e->getMessage());
+  
+  if ( empty($_POST['phone']) || !is_numeric($_POST['phone'])) {
+    print('Номер телефона не указан!<br/>');
+    $errors = TRUE;
+  }
+  
+  if ( empty($_POST['email']) ) {
+    print('Почта не указана!<br/>');
+    $errors = TRUE;
+  }
+  
+  if ( empty($_POST['date']) ) {
+    print('Дата рождения не указана!<br/>');
+    $errors = TRUE;
+  }
+  if ( !isset($_POST['sex']) || !in_array($_POST['sex'], array('male', 'female')) ) {
+    print('Пол не указан!<br/>');
+    $errors = TRUE;
+  }
+  
+  if ( empty($_POST['langs']) ) {
+    print('Языки программирования не выбраны!<br/>');
+    $errors = TRUE;
+  }
+  
+  if (empty($_POST['bio']) ) {
+    print('Заполните биографию.<br/>');
+    $errors = TRUE;
+  }
+  
+  if(!isset($_POST['checkmark']) || $_POST['checkmark'] != 'on') {
+    print('Отметьте чекбокс.<br/>');
+    $errors = TRUE;
+  }
+  
+  if ($errors) {
     exit();
   }
+
+  // Сохранение в базу данных.
   
-  header('Location: ?save=1');
+  $user = 'u67354';
+  $pass = '3075308';
+  $db = new PDO('mysql:host=localhost;dbname=u67354', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+  // Подготовленный запрос. Не именованные метки.
+  try {
+      $stmt = $db->prepare('INSERT INTO form SET name = ?, phone = ?, email = ?, date = ?, sex = ?, bio = ?');
+      $stmt -> execute(array( $_POST['name'], $_POST['phone'], $_POST['email'], $_POST['date'], $_POST['sex'], $_POST['bio'] ));
+    
+      $id = $db->lastInsertId();
+      
+      foreach ($_POST['langs'] as $lang) {
+        $stmt = $db->prepare('INSERT INTO lang_table (id, lang) VALUES (?, ?)');
+        $stmt->execute([$id, $lang]);
+      }
+    }
+    catch(PDOException $e){
+      print('Ошибка: ' . $e->getMessage());
+      exit();
+    }
+    
+    header('Location: ?save=1');
+}
